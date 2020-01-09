250620c92e266013f8e4695a6c1b102a394a7b24
Rithik Bhandari <56810766+rithik-dev@users.noreply.github.com>
2020-01-09 12:56:48 +0530

    Add files via upload
    -added gui using tkinter
    -fixed minor bugs

--- Base Converter.py
+++ Base Converter.py
@@ -1,6 +1,4 @@
-num = input("Enter Number : ").upper()
-from_base = int(input("Enter 'From' Base : "))
-to_base = int(input("Enter 'To' Base : "))
+from tkinter import *
 
 CONVERT = {
     'A': 10,
@@ -45,7 +43,7 @@ def remove_zeroes(num):   # remove leading or trailing zeroes
 
 
 # from_base to decimal
-def from_base_to_decimal(num):
+def from_base_to_decimal(num, from_base):
     is_decimal = "." in num
 
     if from_base != 10:
@@ -70,7 +68,7 @@ def from_base_to_decimal(num):
 
 
 # decimal to to_base
-def decimal_to_to_base(num):
+def decimal_to_to_base(num, to_base):
     is_decimal = "." in num
 
     if to_base != 10:
@@ -101,7 +99,11 @@ def decimal_to_to_base(num):
             precision = 15  # 15 decimal points precision
             for p in range(precision):
                 product = decimal_part * to_base
-                decimal_ans += str(int(product))
+                int_part = int(product)
+                if int_part in CONVERT:
+                    decimal_ans += CONVERT[int_part]
+                else:
+                    decimal_ans += str(int_part)
 
                 if product == 1.0:
                     break
@@ -114,13 +116,60 @@ def decimal_to_to_base(num):
         return num
 
 
-try:
-    ans = from_base_to_decimal(num)
-    ans = decimal_to_to_base(ans)
-    ans = remove_zeroes(ans)
+def gui():
+    num = n.get().strip().upper()
 
-    ans = f"'{num}' in BASE {from_base} is '{ans}' in BASE {to_base}"
+    try:
+        from_base = int(f_base.get())
+        to_base = int(t_base.get())
 
-    print(ans)
-except ValueError:
-    print("Invalid Input !")
+        if from_base>16 or to_base>16:
+            ans = "Invalid Input !"
+        else:
+            ans = from_base_to_decimal(num, from_base)
+            ans = decimal_to_to_base(ans, to_base)
+            ans = remove_zeroes(ans)
+
+    except ValueError:
+        ans = "Invalid Input !"
+
+    answer.set(ans)
+
+
+if __name__ == "__main__":
+    win = Tk()
+    win.geometry('400x200')
+    win.resizable(False, False)
+    win.title("Base Converter")
+
+    Label(win, text='ENTER NUMBER BELOW').pack(side=TOP)
+
+    global n
+    n = Entry(win, justify=CENTER)
+    n.pack(side=TOP, fill=X)
+
+    Label(win, text='ENTER INITIAL BASE BELOW').pack(side=TOP)
+
+    global f_base
+    f_base = Entry(win, justify=CENTER)
+    f_base.pack()
+
+    Label(win, text='ENTER NEW BASE BELOW').pack(side=TOP)
+
+    global t_base
+    t_base = Entry(win, justify=CENTER)
+    t_base.pack()
+
+    Button(win, text='CONVERT', command=gui).pack(side=TOP)
+
+    global answer
+    answer = StringVar()
+    answer.set('')
+
+    Label(win, textvariable=answer, font='Helvetica 16 bold',
+          wraplength=390, justify=CENTER).pack()
+
+    win.mainloop()
+
+
+# FIXME:     23798ABD.389F 16 to 13
